a3ceb97f9f315391cf525af3b281c6801b9ea1a4
Indunil Aravinda (Creative Software) <Indunil.A@CreativeSoftware.com>
2022-07-23 04:10:44 +0530

    Xamarin Practice Project init with completed project

---
+++ MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
+using XamarinTrainingRooms;
+
+namespace InstituteApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [System.Obsolete]
+    public class MainActivity : ListActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+            //Roomrepository from the shared library
+            var repo = new RoomRepository();
+            //receive the list of rooms available
+            var rooms = repo.GetRooms();
+            //set the received available rooms list into a arrayadapter in android
+            var adapter = new ArrayAdapter<XamarinTrainingRooms.XamarinTrainingRooms>(this,Resource.Layout.RoomListItem,rooms.ToArray());
+            //assign the adapter to the ListAdapter of the current instance
+            this.ListAdapter = adapter;
+        }
+        protected override void OnListItemClick(ListView l, View v, int position, long id)
+        {
+            base.OnListItemClick(l, v, position, id);
+            //To pass data between activities
+            var intent = new Intent(this, typeof(TrainingRoomDetailsActivity));
+            //For selecting the id of the item clicked on
+            var selectedItem = ((ArrayAdapter<XamarinTrainingRooms.XamarinTrainingRooms>)ListAdapter).GetItem(position);
+            //pass the selected id through intent and start the activity
+            intent.PutExtra("roomId", selectedItem.id);
+            StartActivity(intent);
+        }
+    }
+}
---
+++ TrainingRoomDetailsActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+
+namespace InstituteApp
+{
+    [Activity(Label = "TrainingRoomDetailsActivity")]
+    public class TrainingRoomDetailsActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+
+            SetContentView(Resource.Layout.TrainingRoomDetailsActivity);
+            //receive the passed selected item id
+            int roomId = Intent.GetIntExtra("roomId", 0);
+            //creatig the room repo to grab the relevant room
+            var repo = new XamarinTrainingRooms.RoomRepository();
+            //selecting the specific room
+            var room = repo.GetRooms(roomId);
+
+            //Customizing the UI
+            this.Title = "Allocated Room Details";
+            this.FindViewById<TextView>(Resource.Id.textViewName).Text = room.name;
+            this.FindViewById<TextView>(Resource.Id.textViewLocation).Text = room.location;
+            
+
+
+        }
+    }
+}
